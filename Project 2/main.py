0cb90fd43154f0badf6c208c1c0952f51883a274
Solrovivrus <77756366+Solrovivrus@users.noreply.github.com>
2021-10-11 21:01:41 -0600

    Add files via upload

---
+++ Project 2/main.py
@@ -0,0 +1,34 @@
+from wumpusWorld import wumpusWorld
+from problemGenerator import problemGenerator
+from simpleExplorer import simpleExplorer
+
+import random
+import numpy as np
+import sys
+from colorama import init
+init(strip=not sys.stdout.isatty()) 
+from termcolor import cprint 
+from pyfiglet import figlet_format
+
+worlds = wumpusWorld
+problem = problemGenerator
+simple = simpleExplorer
+
+cprint(figlet_format('WUMPUS WORLD', font='letters'),
+       'white', 'on_blue', attrs=['bold', 'blink'])
+
+print("Hello! And welcome to the wumpus world. We are going to start by setting up our caves.")
+print("First, give us the probability in which you'd like to encounter pits and wumpi in the caves.")
+
+# getting caves initialized of random sizes 
+caves = worlds.worldGenerator()
+
+#inputs for probability of encountering an obstacle
+pits = input("Probability (0-.9) of generating a pit: ")
+whatsLeft = .9 - float(pits)
+wumpi = input("Probability (0-" + str(whatsLeft) + ") of generating a wumpi: ")
+
+# sends the caves and desired probabilities to generate the worlds appropriately
+caves = problem.generateWorld(caves, wumpi, pits)
+
+simple.placeExplorer(caves[1])
