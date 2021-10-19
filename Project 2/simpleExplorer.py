8dd0d768beac390e857d50297106d8b76319de8c
Derek Logan <75442980+delo9146@users.noreply.github.com>
2021-10-18 20:51:37 -0600

    Update simpleExplorer.py

--- Project 2/simpleExplorer.py
+++ Project 2/simpleExplorer.py
@@ -9,7 +9,27 @@ init(strip=not sys.stdout.isatty())
 from termcolor import cprint 
 from pyfiglet import figlet_format
 
+''' Contributors: Derek Logan
+----------------------------------------------------------------------------------------------
+simpleExplorer takes a list of caves as input and through matrixSelection, sends the cave to be
+explored by the simple explorer one at a time. placeExplorer acts as a sort of 'main' for this 
+class. This is where our secondary tracking matrix is generated and information about where the
+explorer encounters a breeze or stench is found. Information is sent to legalMove to find where 
+the explorer can go and then that is determined at random (while prioritizing safety) in the 
+nextMove method. Our explorer is limited to 5000 moves to explore each cave. We keep track of 
+how much gold was found, how many times killed by wumpus, how many times killed by a pit, how many 
+unsolved caves, how many cells explored and how many wumpi were killed. 
+----------------------------------------------------------------------------------------------
+'''
+
 class simpleExplorer:
+
+    goldFound = 0
+    killedByWumpus = 0
+    killedByPit = 0
+    unsolved = 0
+    numberCells = 0
+    wumpusKilled = 0
 
     # --------------------------------------------------------------------------------------------
     # The caveSelection method iterates through all 10 caves generated and sends them to be solved 
@@ -19,17 +39,36 @@ class simpleExplorer:
         simple = simpleExplorer
         for cave in caves:
             currentCave = simple.placeExplorer(cave)
+        
+        print("The explorer found the gold " + str(simple.goldFound) + " times.")
+        print("The explorer fell into a pit " + str(simple.killedByPit) + " times")
+        print("The explorer was killed by a wumpus " + str(simple.killedByWumpus) + " times")
+
+        return simple.goldFound, simple.killedByPit, simple.killedByWumpus, simple.unsolved, simple.numberCells, simple.wumpusKilled
+        
+        
 
 
+    # --------------------------------------------------------------------------------------------
+    # The placeExplorer method starts the explorer in the cave. The explorer is allowed 100,000 moves
+    # before the game is ended. 
     @staticmethod
     def placeExplorer(cave):
         simple = simpleExplorer
         # initializing starting cell count
         numMoves = 0
+        wumpusKillCount = 0
 
         # placing the explorer in its starting location. Initializing current location tracker
         cave[0,0] = 'E'
         currentLocation = [0,0]
+
+        #resetting wumpi
+        for i in range(len(cave)):
+            for j in range(len(cave[0])):
+                if cave[i,j] == 'DW':
+                    cave[i,j] = 'W'
+
         #assigning arrows based on number of wumpi in cave
         wumpusCount = 0
         for i in range(len(cave)):
@@ -42,7 +81,7 @@ class simpleExplorer:
         tracker = np.full_like(cave, 0, dtype=object)
 
 
-        for i in range(100000000):
+        for i in range(5000):
             cave[currentLocation[0], currentLocation[1]] = 'E'
             print(cave)
             #Process is: Update tracker of what is encountered. Then make move.
@@ -50,15 +89,16 @@ class simpleExplorer:
 
             next = simple.nextMove(tracker, currentLocation)
             numMoves +=1
+            simple.numberCells +=1
             #hopefully killing a wumpus...
             for x in tracker[currentLocation[0], currentLocation[1]]:
                 if "Stench" in x:
-                    tracker, arrows, cave = simple.shootWumpus(cave, currentLocation, tracker, arrows)
+                    tracker, arrows, cave, wumpusKillCount = simple.shootWumpus(cave, currentLocation, tracker, arrows, wumpusKillCount)
 
             #will needs ability to end loop when implemented if return is string
             if isinstance(next, str) == True:
                 print(next)
-                return
+                return wumpusKillCount
             elif cave[next[0], next[1]] == 'DW':
                 print("The cell is blocked by a dead Wumpus!")
                 tracker[next[0], next[1]] = 'DW'
@@ -78,19 +118,26 @@ class simpleExplorer:
                 cprint(figlet_format('The Explorer Fell in a Pit!', font='letters'),
        'white', 'on_grey', attrs=['bold', 'blink'])
                 print("The explorer fell into a pit after " + str(numMoves) + " moves")
+                print("The explorer managed to kill " + str(wumpusKillCount) + " wumpi before passing away")
+                simple.killedByPit += 1
                 return
             elif cave[currentLocation[0], currentLocation[1]] == 'W':
                 cprint(figlet_format('The Explorer was Killed by a Wumpus!!', font='letters'),
        'white', 'on_red', attrs=['bold', 'blink'])
                 print("The explorer has been eaten by a wumpus after " + str(numMoves) + " moves")
+                print("The explorer managed to kill " + str(wumpusKillCount) + " wumpi before passing away")
+                simple.killedByWumpus += 1
                 return
             elif cave[currentLocation[0], currentLocation[1]] == 'G':
                 cprint(figlet_format('GOLD FOUND', font='letters'),
        'white', 'on_yellow', attrs=['bold', 'blink'])
                 print("The explorer has found the gold in " + str(numMoves) + " moves")
+                print("The explorer managed to kill " + str(wumpusKillCount) + " wumpi before finding the gold")
+                simple.goldFound += 1
                 return
             else:
                 print("The explorer safely moves to the next cell :) ")
+        simple.unsolved +=1
 
 
     # --------------------------------------------------------------------------------------------
@@ -151,7 +198,9 @@ class simpleExplorer:
         
         return tracker
 
-
+    # --------------------------------------------------------------------------------------------
+    # The nextMove method finds the explorers next move. It populates a list that was returned from
+    # legalMove with any possible moves and then randomly selects one.
     @staticmethod
     def nextMove(tracker, currentLocation):
         simple = simpleExplorer
@@ -201,12 +250,11 @@ class simpleExplorer:
 
         # if list isn't full, pick one that's not been explored first
         randomChoice = random.randint(0, len(moves)-1)
-        print("Move choice =" + str(randomChoice))
         next.append(moves[randomChoice][0])
         next.append(moves[randomChoice][1])
         return next
 
-            
+    # --------------------------------------------------------------------------------------------
     # the legalMove function checks through the neighbors to see what exists and adds to 
     # legalList with what directions would work
     @staticmethod
@@ -227,9 +275,12 @@ class simpleExplorer:
             legalList.append([x+1, y])
         
         return legalList
-        
+
+    # --------------------------------------------------------------------------------------------
+    # the shootWumpus method fires the arrow for the explorer. It will stop when it hits a wumpus,
+    # a dead wumpus, or the edge of the cave
     @staticmethod
-    def shootWumpus(cave, currentLocation, tracker, arrowCount):
+    def shootWumpus(cave, currentLocation, tracker, arrowCount, wumpusKillCount):
         simple = simpleExplorer
         caveDim= np.shape(cave)
         directionList = simple.legalMove(tracker, currentLocation)
@@ -246,16 +297,18 @@ class simpleExplorer:
                 print("The arrow hit a wumpus ")
                 cave[arrowDirection[0], arrowDirection[1]] = 'DW'
                 print("The explorer hears a terrible scream! A wumpus is dead!")
+                wumpusKillCount +=1
+                simple.wumpusKilled +=1
                 for x in tracker[currentLocation[0], currentLocation[1]]:
                     if "Stench" in x:
                         tracker[currentLocation[0], currentLocation[1]].remove(x)
                         arrowCount -= 1
-                        return tracker, arrowCount, cave
+                        return tracker, arrowCount, cave, wumpusKillCount
             
             elif cave[arrowDirection[0], arrowDirection[1]] == 'DW':
                 print("The explorer hears a mushy thud... I hit a dead Wumpus!")
                 arrowCount -= 1
-                return tracker, arrowCount, cave
+                return tracker, arrowCount, cave, wumpusKillCount
 
             # indicate that arrow traveling up and continues
             elif currentLocation[0] > arrowDirection[0] and arrowDirection[0] > 0:
@@ -275,4 +328,24 @@ class simpleExplorer:
                 print("The arrow travels to the right and is in cell " + str(arrowDirection))
             else:
                 print("The explorer hears a thud as the arrow collides with a wall. Better aim next time!")
-                return tracker, arrowCount, cave
+                return tracker, arrowCount, cave, wumpusKillCount
+
+            
+
+
+
+
+
+
+
+        
+        
+        
+
+
+
+            
+
+
+
+        
